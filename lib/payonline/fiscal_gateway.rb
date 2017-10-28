682bd73580681c5fb0ad96d87aed4763c58fee3d
SumLare <anohin56n@gmail.com>
2017-10-28 21:12:02 +0300

    Create fiscal gateway

---
+++ lib/payonline/fiscal_gateway.rb
@@ -0,0 +1,38 @@
+module Payonline
+  class FiscalGateway
+    BASE_URL = 'https://secure.payonlinesystem.com/Services/Fiscal/Request.ashx'
+    SIGNED_PARAMS = %w(request_body)
+
+    def initialize(params = {})
+      @params = prepare_params(params)
+    end
+
+    # Perform the fiscal operation and return the response
+    def fiscalization
+      response = HTTParty.post(fiscal_url)
+      return false unless response.success?
+
+      Payonline::FiscalResponse.new(response.body).success?
+    end
+
+    # Return the URL without performing a request
+    def fiscal_url
+      security_key = Payonline::Signature.new(@params, SIGNED_PARAMS).digest
+
+      "#{BASE_URL}/?#{fiscal_url_params.to_query}"
+    end
+
+    private
+
+    def fiscal_url_params
+      params = { security_key: security_key, merchant_id: Payonline.configuration.merchant_id }
+      params.transform_keys { |key| key.to_s.camelize }
+    end
+
+    def prepare_params(params)
+      params
+        .with_indifferent_access
+        .deep_merge({ request_body: { totalAmount: format('%.2f', params[:request_body][:totalAmount]) } })
+    end
+  end
+end

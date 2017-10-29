1e691011be38fbd52726c7933e8a32570690b5cf
SumLare <anohin56n@gmail.com>
2017-10-29 14:00:08 +0300

    Fix response params parsing

--- lib/payonline/fiscal_response.rb
+++ lib/payonline/fiscal_response.rb
@@ -17,10 +17,7 @@ module Payonline
     private
 
     def parse_response(response)
-      CGI.parse(response)
-        .transform_keys { |key| key.to_s.underscore }
-        .transform_values(&:first)
-        .with_indifferent_access
+      JSON.parse(response).with_indifferent_access
     end
   end
 end
